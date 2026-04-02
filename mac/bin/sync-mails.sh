017787b8b60449157743cd6ba205aa5b093144e6
HannesT117 <12032626+HannesT117@users.noreply.github.com>
2026-04-02 18:34:25 +0200

    refactor(mac): move mails to dot folder

--- mac/bin/sync-mails.sh
+++ mac/bin/sync-mails.sh
@@ -1,6 +1,6 @@
 # Sync local mail setup with remotes
 
 source $HOME/.zsecrets
-# to delete old mails in trash: find ~/Maildir/uberspace/Trash/cur -type f -mtime +90 -delete # >> /Users/johannes/mail.log 2>&1
+# to delete old mails in trash: find ~/.Maildir/uberspace/Trash/cur -type f -mtime +90 -delete # >> /Users/johannes/mail.log 2>&1
 mbsync -a # >> /Users/johannes/mail.log 2>&1
 notmuch new
